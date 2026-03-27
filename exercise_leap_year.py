d53f267369e9a2dfcc7cb84b21b90bbb36531034
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-03-27 12:25:18 -0300

    Update exercise_leap_year.py

--- exercise_leap_year.py
+++ exercise_leap_year.py
@@ -18,3 +18,10 @@ def leap_year():
         El año 1700 no es bisiesto
     """
     pass
+
+    año=int(input())
+
+    if año % 4 == 0 and año % 100 != 0 or año % 400 == 0:
+        print (f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
